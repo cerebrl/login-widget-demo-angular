18d82817f45b4bdda5f096c2304caf577bce74ea
Justin Lowery <justin.lowery@forgerock.com>
2023-04-21 17:31:48 -0500

    Completion of quick start tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Widget, {
+  component,
+  configuration,
+  journey,
+  user,
+} from '@forgerock/login-widget';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'login-widget-angular';
+
+  config: ReturnType<typeof configuration> | undefined;
+  componentEvents: ReturnType<typeof component> | undefined;
+  journeyEvents: ReturnType<typeof journey> | undefined;
+
+  unsubJourneyEvents: () => void = () => {};
+
+  userInfo: any;
+
+  ngOnInit(): void {
+    this.config = configuration();
+    this.componentEvents = component();
+    this.journeyEvents = journey();
+
+    this.config.set({
+      forgerock: {
+        serverConfig: {
+          baseUrl: 'https://openam-crbrl-01.forgeblocks.com/am',
+          timeout: 3000,
+        },
+      },
+    });
+
+    this.unsubJourneyEvents = this.journeyEvents?.subscribe((event) => {
+      if (this.userInfo !== event.user.response) {
+        this.userInfo = event.user.response;
+      }
+    });
+
+    const widgetRootEl = document.getElementById(
+      'widget-root'
+    ) as HTMLDivElement;
+
+    new Widget({ target: widgetRootEl });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubJourneyEvents();
+  }
+
+  login(): void {
+    this.journeyEvents?.start();
+    this.componentEvents?.open();
+  }
+
+  logout(): void {
+    user.logout();
+  }
 }
